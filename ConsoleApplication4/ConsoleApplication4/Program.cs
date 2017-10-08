14e42ccefcd0751c789cf56349979daab0425d4a
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 14:31:23 +0200

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5+22);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 6;
+
+            double c = 2.344, d = -4.223;
+
+            float e = 3.455f, g;
+            g = 234.3433f;
+
+            Console.WriteLine("a+c= {0}", a + c);
+            Console.WriteLine("b-d= {0}", b - d);
+            Console.WriteLine("c*b= {0}", c * b);
+            Console.WriteLine("b/a= {0}", b / a);
+
+            Console.WriteLine("a%b ={0}", a % b);
+
+            Console.ReadKey();
+        }
+    }
+}
