4724b91cf6dd4d8df0bd2c749fc65408332e0805
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 14:38:17 +0200

    Add files via upload

---
+++ ConsoleApplication20/ConsoleApplication20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int suma=0;
+            //for(int i=1;i<=n;i++)
+            //{
+            //    suma += i;
+            //}
+
+            //int j = 1;
+            //while(j<=n)
+            //{
+            //    suma += j;
+            //    j++;
+            //}
+
+            int k =1;
+            do
+            {
+                suma += k;
+                k++;
+            }
+            while (k <= n);
+
+        }
+    }
+}
---
+++ ConsoleApplication21/ConsoleApplication21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj symbol temp");
+            ConsoleKeyInfo klawisz = Console.ReadKey();
+            char znak = klawisz.KeyChar;
+
+            Console.WriteLine(znak);
+
+            Console.ReadKey();
+        }
+    }
+}
