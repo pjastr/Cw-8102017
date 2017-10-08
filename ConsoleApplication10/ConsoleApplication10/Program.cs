4fe472356e54a7c34b542c3e89fc7be337e26c43
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 14:34:35 +0200

    Add files via upload

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj trzecią liczbę");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a >= b)
+            {
+                if (a<=c)
+                {
+                    Console.WriteLine("Max = {0}, Min ={1}", c, b);
+                }
+                else if (b<=c)
+                {
+                    Console.WriteLine("Max = {0}, Min ={1}", a, b);
+                }
+                else
+                {
+                    Console.WriteLine("Max = {0}, Min ={1}", a, c);
+                }
+            }
+            else
+            {
+                if (b <= c)
+                {
+                    Console.WriteLine("Max = {0}, Min ={1}", c, a);
+                }
+                else if (a <= c)
+                {
+                    Console.WriteLine("Max = {0}, Min ={1}", b, a);
+                }
+                else
+                {
+                    Console.WriteLine("Max = {0}, Min ={1}", b, c);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            //int a = 1;
+            int suma = 0;
+            //while (a <= n)
+            //{
+            //    suma += a;
+            //    a++;
+            //}
+
+            //for (int i = 1; i <= n;i++ )
+            //{
+            //    suma += i;
+            //}
+
+            int j = 1;
+            do
+            {
+                suma+=j;
+                j++;
+            }
+            while (j<=n);
+
+
+                Console.Write("suma = {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
