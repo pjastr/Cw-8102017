ff6a950c8e904a37cf25b76a9cdfa7a753cd3233
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 14:37:24 +0200

    Add files via upload

---
+++ ConsoleApplication17/ConsoleApplication17/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        //static int ObliczSume(int a,int b)
+        //{
+        //    return a + b;
+        //}
+
+        static void WyswietlSume(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        //bradzo zła praktyka!!
+        static int ObliczIWyswietlSume(int a,int b)
+        {
+            int suma = a + b;
+            Console.WriteLine(suma);
+            return suma;
+        }
+        
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            /*
+            int max = a;
+            if(b>max) max = b;
+            if(c>max) max = c;
+
+            int min = a;
+            if(b<min) min = b;
+            if(c<min) min = c;
+
+            Console.WriteLine("Najwyższa: " + max +" Najniższa: " + min);
+            */
+
+            //int[] arr = new int[] { a, b, c };
+            //Console.WriteLine("Max" + arr.Max());
+            //Console.WriteLine("Min" + arr.Min());
+
+            Console.WriteLine("Max= {0}", Math.Max(Math.Max(a,b),c));
+            Console.WriteLine("Min= {0}", Math.Min(Math.Min(a, b), c));
+            Console.ReadKey();
+
+
+
+            
+
+           
+            }
+        
+    }
+}
---
+++ ConsoleApplication19/ConsoleApplication19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj znak");
+            int numerZnaku = Console.Read();
+            char znak = (char)numerZnaku;
+            Console.ReadLine();
+            Console.WriteLine("podaj liczbę");
+            int liczba = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+
+        }
+    }
+}
